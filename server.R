e2d11cbd13c07cf8bd878322075bbd7b94828a4e
Dennis Prangle <dennis.prangle@gmail.com>
2018-01-17 15:29:06 +0000

    Simplify by not using exams package

--- server.R
+++ server.R
@@ -1,25 +1,27 @@
-lookup<-c("1.2 Bayes Theorem"="BayesProbability.Rmd","1.2 Bayes Theorem 2"="BayesProbability.Rmd","1.3 Maximum Likelihood"="MaximumLikelihood.Rmd","2.1 Prior Density Plots"="PriorDensityPlots.Rmd","2.1 Prior/Posterior Probabilities"="PriorPosteriorProbabilities.Rmd","2.2 Exponential Prior"="ExponentialPrior.Rmd","2.2 Normal Prior"="NormalPrior.Rmd","3.3 Truncated Distributions"="TruncatedDistributions.Rmd","3.5 Jeffreys Prior"="JeffreysPrior.Rmd","3.6 Asymptotic Posterior"="AsymptoticPosterior.Rmd","4.1 Credible Intervals"="CredibleIntervals.Rmd","4.1 Discrete HDI"="DiscreteHDI.Rmd","4.1 HDI 1"="HDIntervals.Rmd","4.1 HDI 2"="HDIntervals2.Rmd","4.1 HDI 3"="HDIntervals3.Rmd","4.2 Predictive Distributions"="PredictiveDistributions.Rmd","4.2 Predictive Probability Function"="PredictiveProbFunc.Rmd","4.3 Sequential Use of Bayes Theorem"="SequentialBayes.Rmd")
-library(exams)
-library(rmarkdown)
+library(markdown)
+library(knitr)
 library(shiny)
-library(shinythemes)
-library(shinyjs)
-library(V8)
+
+example_lookup = c("1.2 Bayes Theorem"="BayesProbability.Rmd","1.2 Bayes Theorem 2"="BayesProbability2.Rmd","1.3 Maximum Likelihood"="MaximumLikelihood.Rmd","2.1 Prior Density Plots"="PriorDensityPlots.Rmd","2.1 Prior/Posterior Probabilities"="PriorPosteriorProbabilities.Rmd","2.2 Exponential Prior"="ExponentialPrior.Rmd","2.2 Normal Prior"="NormalPrior.Rmd","3.3 Truncated Distributions"="TruncatedDistributions.Rmd","3.5 Jeffreys Prior"="JeffreysPrior.Rmd","3.6 Asymptotic Posterior"="AsymptoticPosterior.Rmd","4.1 Credible Intervals"="CredibleIntervals.Rmd","4.1 Discrete HDI"="DiscreteHDI.Rmd","4.1 HDI 1"="HDIntervals.Rmd","4.1 HDI 2"="HDIntervals2.Rmd","4.1 HDI 3"="HDIntervals3.Rmd","4.2 Predictive Distributions"="PredictiveDistributions.Rmd","4.2 Predictive Probability Function"="PredictiveProbFunc.Rmd","4.3 Sequential Use of Bayes Theorem"="SequentialBayes.Rmd")
+solution_lookup = c("1.2 Bayes Theorem"="BayesProbability_sol.Rmd","1.2 Bayes Theorem 2"="BayesProbability2_sol.Rmd","1.3 Maximum Likelihood"="MaximumLikelihood_sol.Rmd","2.1 Prior Density Plots"="PriorDensityPlots_sol.Rmd","2.1 Prior/Posterior Probabilities"="PriorPosteriorProbabilities_sol.Rmd","2.2 Exponential Prior"="ExponentialPrior_sol.Rmd","2.2 Normal Prior"="NormalPrior_sol.Rmd","3.3 Truncated Distributions"="TruncatedDistributions_sol.Rmd","3.5 Jeffreys Prior"="JeffreysPrior_sol.Rmd","3.6 Asymptotic Posterior"="AsymptoticPosterior_sol.Rmd","4.1 Credible Intervals"="CredibleIntervals_sol.Rmd","4.1 Discrete HDI"="DiscreteHDI_sol.Rmd","4.1 HDI 1"="HDIntervals_sol.Rmd","4.1 HDI 2"="HDIntervals2_sol.Rmd","4.1 HDI 3"="HDIntervals3_sol.Rmd","4.2 Predictive Distributions"="PredictiveDistributions_sol.Rmd","4.2 Predictive Probability Function"="PredictiveProbFunc_sol.Rmd","4.3 Sequential Use of Bayes Theorem"="SequentialBayes_sol.Rmd")
+
 shinyServer(function(input,output){
   seed <- as.numeric(Sys.time())  
   
-  makeQuestion <- function(ex) {
+  makeQuestion <- function(x) {
     set.seed(seed)
-    exams2html(ex, mathjax = TRUE, solution = FALSE, dir = "question", template="plain1.html", converter = "pandoc",quiet=TRUE)
-    withMathJax(includeHTML("question/plain11.html"))
+    #TO DO: create unique temp filename to knit to. Ensure we delete this file below.
+    ex_file = knit(example_lookup[x], quiet = TRUE)
+    question = HTML(markdownToHTML(ex_file))
+    withMathJax(question)
   }
-  makeSolution <- function(ex) {
+  makeSolution <- function(x) {
     set.seed(seed)
-    exams2html(ex, mathjax = TRUE, question = FALSE, dir = "solution", template="plain1.html", converter = "pandoc", quiet=TRUE)
-    withMathJax(includeHTML("solution/plain11.html"))
+    #TO DO: create unique temp filename to knit to. Ensure we delete this file below.
+    sol_file = knit(solution_lookup[x], quiet=TRUE)
+    solution = HTML(markdownToHTML(sol_file))
+    withMathJax(solution)
   }
-  output$question <- renderUI({makeQuestion(input$var)})
-  output$solution <- renderUI({makeSolution(input$var)})
-  observeEvent(input$refresh, {shinyjs::js$refresh()})
-  observeEvent(input$var, {shinyjs::reset("sol")})
+  output$question <- renderUI({makeQuestion(input$example)})
+  output$solution <- renderUI({makeSolution(input$example)})
 })
--- ui.R
+++ ui.R
@@ -1,18 +1,17 @@
-lookup<-c("1.2 Bayes Theorem"="BayesProbability.Rmd","1.2 Bayes Theorem 2"="BayesProbability2.Rmd","1.3 Maximum Likelihood"="MaximumLikelihood.Rmd","2.1 Prior Density Plots"="PriorDensityPlots.Rmd","2.1 Prior/Posterior Probabilities"="PriorPosteriorProbabilities.Rmd","2.2 Exponential Prior"="ExponentialPrior.Rmd","2.2 Normal Prior"="NormalPrior.Rmd","3.3 Truncated Distributions"="TruncatedDistributions.Rmd","3.5 Jeffreys Prior"="JeffreysPrior.Rmd","3.6 Asymptotic Posterior"="AsymptoticPosterior.Rmd","4.1 Credible Intervals"="CredibleIntervals.Rmd","4.1 Discrete HDI"="DiscreteHDI.Rmd","4.1 HDI 1"="HDIntervals.Rmd","4.1 HDI 2"="HDIntervals2.Rmd","4.1 HDI 3"="HDIntervals3.Rmd","4.2 Predictive Distributions"="PredictiveDistributions.Rmd","4.2 Predictive Probability Function"="PredictiveProbFunc.Rmd","4.3 Sequential Use of Bayes Theorem"="SequentialBayes.Rmd")
 library(shiny)
 library(shinythemes)
-library(exams)
 library(rmarkdown)
-library(shinyjs)
-shinyUI(fluidPage(
-  shinyjs::useShinyjs(),
-  shinyjs::extendShinyjs(text="shinyjs.refresh=function() {location.reload();}"),
-  theme = shinytheme("sandstone"),
-                selectInput(inputId="var",label="Choose a Topic",choices=lookup,selected="BayesProbability.Rmd"),
+# Full list of examples
+# examples = c("1.2 Bayes Theorem", "1.2 Bayes Theorem 2", "1.3 Maximum Likelihood", "2.1 Prior Density Plots", "2.1 Prior/Posterior Probabilities", "2.2 Exponential Prior", "2.2 Normal Prior", "3.3 Truncated Distributions", "3.5 Jeffreys Prior", "3.6 Asymptotic Posterior", "4.1 Credible Intervals", "4.1 Discrete HDI", "4.1 HDI 1", "4.1 HDI 2", "4.1 HDI 3", "4.2 Predictive Distributions", "4.2 Predictive Probability Function", "4.3 Sequential Use of Bayes Theorem")
+# Those which are polished and available
+examples = c("1.2 Bayes Theorem", "1.2 Bayes Theorem 2")
+
+# TO DO: add a warning that math symbols will take a few seconds to load
+shinyUI(fluidPage(theme = shinytheme("sandstone"),
+                selectInput(inputId="example", label="Choose a Topic", choices=examples, selected="BayesProbability.Rmd"),
                 actionButton(inputId="show",label="Reveal Answers"),
-                actionButton(inputId="refresh",label="New Question Set"),
                 mainPanel(
                   htmlOutput("question", header=""),
-                  conditionalPanel(condition="input.show%2 == 1", htmlOutput("solution"), header="",label="sol")
+                  conditionalPanel(condition="input.show%2 == 1", htmlOutput("solution"), header="")
                 )
 ))
