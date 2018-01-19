650cdfefe1b2edfbd257e50af7093e92ec9f138f
Dennis Prangle <dennis.prangle@gmail.com>
2018-01-19 09:45:03 +0000

    Refresh seed when example changes

--- server.R
+++ server.R
@@ -6,7 +6,7 @@ example_lookup = c(" "="blank.Rmd", "1.2 Bayes Theorem"="BayesProbability.Rmd", 
 solution_lookup = c(" "="blank.Rmd", "1.2 Bayes Theorem"="BayesProbability_sol.Rmd", "1.2 Bayes Theorem 2"="BayesProbability2_sol.Rmd","1.3 Maximum Likelihood"="MaximumLikelihood_sol.Rmd","2.1 Prior Density Plots"="PriorDensityPlots_sol.Rmd","2.1 Prior/Posterior Probabilities"="PriorPosteriorProbabilities_sol.Rmd","2.2 Exponential Prior"="ExponentialPrior_sol.Rmd","2.2 Normal Prior"="NormalPrior_sol.Rmd","3.3 Truncated Distributions"="TruncatedDistributions_sol.Rmd","3.5 Jeffreys Prior"="JeffreysPrior_sol.Rmd","3.6 Asymptotic Posterior"="AsymptoticPosterior_sol.Rmd","4.1 Credible Intervals"="CredibleIntervals_sol.Rmd","4.1 Discrete HDI"="DiscreteHDI_sol.Rmd","4.1 HDI 1"="HDIntervals_sol.Rmd","4.1 HDI 2"="HDIntervals2_sol.Rmd","4.1 HDI 3"="HDIntervals3_sol.Rmd","4.2 Predictive Distributions"="PredictiveDistributions_sol.Rmd","4.2 Predictive Probability Function"="PredictiveProbFunc_sol.Rmd","4.3 Sequential Use of Bayes Theorem"="SequentialBayes_sol.Rmd")
 
 shinyServer(function(input,output){
-  seed = as.numeric(Sys.time())
+  seed = 0
   makeHTML = function(filename) {
     set.seed(seed)
     #TO DO: create unique temp filename to knit to. Ensure we delete this file below.
@@ -14,6 +14,7 @@ shinyServer(function(input,output){
     withMathJax(includeHTML("temp.html"))
   }
   observeEvent(input$example, {
+    seed <<- as.numeric(Sys.time())
     output$question = renderUI({makeHTML(example_lookup[input$example])})
     output$solution = renderUI({makeHTML("blank.Rmd")})
   })
