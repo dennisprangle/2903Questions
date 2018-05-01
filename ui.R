7c2b41991787a141e4d22a9830c7dadf5e989bb1
Dennis Prangle <dennis.prangle@gmail.com>
2018-05-01 16:54:30 +0100

    Add truncated distributions question

--- ui.R
+++ ui.R
@@ -3,9 +3,10 @@ library(shinythemes)
 library(rmarkdown)
 # Current completed examples
 examples = c(" ", "1.2 Bayes Theorem", "1.2 Bayes Theorem 2", "1.3 Maximum Likelihood",
-             "2.1 Prior/Posterior Probabilities", "2.2 Exponential Prior")
+             "2.1 Prior/Posterior Probabilities", "2.2 Exponential Prior",
+             "3.3 Truncated Distributions")
 # Remaining examples to add
-# "2.2 Normal Prior", "3.3 Truncated Distributions", "3.6 Asymptotic Posterior", "4.1 Credible Intervals", "4.1 Discrete HDI", "4.1 HDI", "4.2 Predictive Distributions", "4.2 Predictive Probability Function", "4.3 Sequential Use of Bayes Theorem")
+# "2.2 Normal Prior", "3.6 Asymptotic Posterior", "4.1 Credible Intervals", "4.1 Discrete HDI", "4.1 HDI", "4.2 Predictive Distributions", "4.2 Predictive Probability Function", "4.3 Sequential Use of Bayes Theorem")
 
 # TO DO: add a warning that math symbols will take a few seconds to load
 shinyUI(fluidPage(theme = shinytheme("sandstone"),
